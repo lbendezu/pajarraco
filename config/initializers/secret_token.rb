23767b300f1260b9de9b866779679e446f4e98a0
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-03-10 11:46:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pajarraco::Application.config.secret_token = '44b32fa693320320c3bf7bcb8aabb37af64e5a1f585b69cddb2ba1aeb39bbb286497b6c86f1d9403a7fd16c59be455e3d7d2d4785955c971b573010c6728f430'
