4d7be6d7948607e4e86f9c4f4733c957bf869086
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-03-17 17:59:48 -0500

    refactor and new seed

--- app/assets/javascripts/ember/helpers/linkify.js
+++ app/assets/javascripts/ember/helpers/linkify.js
@@ -3,6 +3,6 @@ Handlebars.registerHelper('linkify', function (property) {
       exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
       linked = '';
   
-  linked = text.replace(exp, '<a target="_blank" href="$1">$1</a>');
+  linked = text.replace(exp, '<a href="$1" target="_blank">$1</a>');
   return new Handlebars.SafeString(linked);
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Tweet.delete_all
 
 if Rails.env.development?
   Tweet.create(
-    text:       "nice talk @dbrady! #mwrc",
+    text:       "#emberjs + #rails demo at @epicperu http://pajarraco.herokuapp.com ... ready for How to and ember data :)",
     user_nick:  "frodsan",
     user_name:  "Francesco Rodriguez",
     user_image: "https://twimg0-a.akamaihd.net/profile_images/1850899605/Photo_on_2-24-12_at_1.05_PM.jpg"
