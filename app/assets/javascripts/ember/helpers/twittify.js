785f351ab95f69e1ff7517328c1d6fc8c26f7102
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-03-30 21:26:13 -0500

    just one var statement

--- app/assets/javascripts/ember/helpers/twittify.js
+++ app/assets/javascripts/ember/helpers/twittify.js
@@ -1,7 +1,6 @@
 Handlebars.registerHelper('twittify', function (property) {
-  var text = Ember.getPath(this, property);
-  
-  var twitterParser = new TwitterParser(text);
+  var text = Ember.getPath(this, property),
+      twitterParser = new TwitterParser(text);
 
   return new Handlebars.SafeString(
     twitterParser.parseURL()
